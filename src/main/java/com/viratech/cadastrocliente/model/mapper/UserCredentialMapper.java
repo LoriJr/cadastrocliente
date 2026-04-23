9fc1b1c706e6061f9fa7c0ec37eaa3e7956ca53c
lorijr <jrsilva.lorivas@gmail.com>
2026-04-22 23:23:35 -0300

    feat: add ConfigSecurity and UserCredentialMapper

---
+++ src/main/java/com/viratech/cadastrocliente/model/mapper/UserCredentialMapper.java
@@ -0,0 +1,28 @@
+package com.viratech.cadastrocliente.model.mapper;
+
+import com.viratech.cadastrocliente.dto.UserCredentialRequestDTO;
+import com.viratech.cadastrocliente.model.entity.UserCredential;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.Named;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public abstract class UserCredentialMapper {
+
+    @Autowired
+    protected PasswordEncoder passwordEncoder;
+
+    @Mapping(target= "id", ignore = true)
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "username", source = "email")
+    @Mapping(target = "password", source = "password", qualifiedByName = "encryptPassword")
+    public abstract UserCredential toEntity(UserCredentialRequestDTO request);
+
+    @Named("encryptPassword")
+    protected String encryptPassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+}
