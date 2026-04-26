14fef801c8961037f7d3d8f1e61073745364034b
lorijr <jrsilva.lorivas@gmail.com>
2026-04-25 23:58:27 -0300

    feat: add saveCredential

--- src/main/java/com/viratech/cadastrocliente/controller/UserCredentialController.java
+++ src/main/java/com/viratech/cadastrocliente/controller/UserCredentialController.java
@@ -7,17 +7,26 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("api/v1/auth")
 public class UserCredentialController {
 
     private final UserCredentialService credentialService;
 
-    @PostMapping
+    @PostMapping("/register")
     public ResponseEntity<UserCredentialResponseDTO> saveCredential(@RequestBody @Valid UserCredentialRequestDTO request){
-        //TODO verificar lógica de consulta de usuário pelo email no service
-        return null;
+
+        UserCredentialResponseDTO response = credentialService.saveUserCredential(request);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{email}")
+                .buildAndExpand(response.email())
+                .toUri();
+        return ResponseEntity.created(uri).body(response);
     }
 }
--- src/main/java/com/viratech/cadastrocliente/dto/UserCredentialRequestDTO.java
+++ src/main/java/com/viratech/cadastrocliente/dto/UserCredentialRequestDTO.java
@@ -1,7 +1,15 @@
 package com.viratech.cadastrocliente.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record UserCredentialRequestDTO(
+
+        @NotBlank
+        @Email(message = "error.email.invalid")
         String email,
+
+        @NotBlank
         String password
 ) {
 }
