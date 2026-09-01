3a6067986287b797c57d68273a87d90ea33ec7ad
lorijr <jrsilva.lorivas@gmail.com>
2026-09-01 15:42:34 -0300

    chore: add shouldValidateFieldBlank with parameterized

--- src/test/java/com/viratech/cadastrocliente/dto/UserRequesDTOTest.java
+++ src/test/java/com/viratech/cadastrocliente/dto/UserRequesDTOTest.java
@@ -6,7 +6,7 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
-import org.assertj.core.api.Assertions;
+import org.assertj.core.util.Strings;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -27,62 +27,73 @@ public class UserRequesDTOTest {
     private Validator validator;
 
     @BeforeEach
-    void setUp() { ValidatorFactory factory = Validation.buildDefaultValidatorFactory(); validator = factory.getValidator(); }
-
-    @ParameterizedTest
-    @MethodSource("dataProvider")
-    @DisplayName("Deve Validar Campos em Branco")
-    public void shouldValidateFieldBlank(String name, String email, String phone, String cpf, String rg, String field) {
-
-        UserRequestDTO requestDTO = UserRequestDtoBuilder.aUserRequestDTO().name(name).email(email).phone(phone).cpf(cpf).rg(rg).birthDate(LocalDate.of(1900, 1, 1)).address(AddressBuilder.aAddress().nowDTO()).now();
-
-        Set<ConstraintViolation<UserRequestDTO>> violations =
-                validator.validate(requestDTO);
-
-        assertTrue(
-                violations.stream()
-                        .anyMatch(v ->
-                                v.getPropertyPath().toString().equals(field)
-                        )
-        );
-
-
-    }
-
-    private static Stream<Arguments> dataProvider() {
-        return Stream.of(
-                Arguments.of(null, "email@email", "11912341234", "12345678912", "121234567", LocalDate.of(1900, 1, 1), aAddress().nowDTO()),
-                Arguments.of("Usuario Valido", null, "11912341234", "12345678912", "121234567", LocalDate.of(1900, 1, 1), aAddress().nowDTO()),
-                Arguments.of("Usuario Valido", "email@email", null, "12345678912", "121234567", LocalDate.of(1900, 1, 1), aAddress().nowDTO()),
-                Arguments.of("Usuario Valido", "email@email", "11912341234", null, "121234567", LocalDate.of(1900, 1, 1), aAddress().nowDTO()),
-                Arguments.of("Usuario Valido", "email@email", "11912341234", "12345678912", null, LocalDate.of(1900, 1, 1), aAddress().nowDTO()),
-                Arguments.of("Usuario Valido", "email@email", "11912341234", "12345678912", "121234567", null, aAddress().nowDTO()),
-                Arguments.of("Usuario Valido", "email@email", "11912341234", "12345678912", "121234567", LocalDate.of(1900, 1, 1), null)
-        );
-
+    void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
     }
 
     @Test
-    public void shouldValidateNameBlank(){
-        UserRequestDTO requestDTO = UserRequestDtoBuilder
-                .aUserRequestDTO()
-                .name(" ")
-                .now();
-
-        var violations = validator.validate(requestDTO);
-
-        assertThat(violations)
-                .anyMatch(v -> v.getPropertyPath().toString().equals("name"));
-    }
-
-    @Test
-    @DisplayName("Deve criar usuário com todos os campos válidos")
-    public void shouldCreateValidUserRequest(){
-
+    @DisplayName("Deve aceitar request válido")
+    public void shouldCreateValidUserRequest() {
         UserRequestDTO requestDTO = UserRequestDtoBuilder.aUserRequestDTO().now();
+
         var violations = validator.validate(requestDTO);
         assertThat(violations).isEmpty();
     }
 
+    @ParameterizedTest(name = "{5}")
+    @MethodSource("dataProvider")
+    @DisplayName("Deve Validar Campos @NotBlank")
+    public void shouldValidateFieldBlank(String name, String email, String phone, String cpf, String rg, String fieldMessage) {
+
+        UserRequestDTO requestDTO = UserRequestDtoBuilder.aUserRequestDTO()
+                .name(name)
+                .email(email)
+                .phone(phone)
+                .cpf(cpf)
+                .rg(rg)
+                .birthDate(LocalDate.of(1900, 1, 1))
+                .address(AddressBuilder.aAddress().nowDTO())
+                .now();
+
+        Set<ConstraintViolation<UserRequestDTO>> violations =
+                validator.validate(requestDTO);
+
+        assertThat(violations.stream()
+                        .anyMatch(v ->
+                                v.getPropertyPath().toString().equals(fieldMessage)
+                        )
+        );
+//        var violat = validator.validateProperty(requestDTO, "email");
+//        assertThat(violat).isNotEmpty();
+    }
+
+    private static Stream<Arguments> dataProvider() {
+
+        return Stream.of(
+                Arguments.of(null, "usuario@email.com", "11911112222", "52998224725", "424214181", "name null"),
+                Arguments.of("", "usuario@email.com", "11911112222", "52998224725", "424214181", "name empty"),
+                Arguments.of(" ", "usuario@email.com", "11911112222", "52998224725", "424214181", "name blank"),
+
+                Arguments.of("Usuario Valido", null, "11911112222", "52998224725", "424214181", "email null"),
+                Arguments.of("Usuario Valido", "", "11911112222", "52998224725", "424214181", "email empty"),
+                Arguments.of("Usuario Valido", " ", "11911112222", "52998224725", "424214181", "email blank"),
+
+                Arguments.of("Usuario Valido", "usuario@email.com", null, "52998224725", "424214181", "phone null"),
+                Arguments.of("Usuario Valido", "usuario@email.com", "", "52998224725", "424214181", "phone empty"),
+                Arguments.of("Usuario Valido", "usuario@email.com", " ", "52998224725", "424214181", "phone blank"),
+
+                Arguments.of("Usuario Valido", "usuario@email.com", "11911112222", null, "424214181", "cpf null"),
+                Arguments.of("Usuario Valido", "usuario@email.com", "11911112222", "", "424214181", "cpf empty"),
+                Arguments.of("Usuario Valido", "usuario@email.com", "11911112222", " ", "424214181", "cpf blank"),
+
+                Arguments.of("Usuario Valido", "usuario@email.com", "11911112222", "52998224725", null, "rg null"),
+                Arguments.of("Usuario Valido", "usuario@email.com", "11911112222", "52998224725", "", "rg empty"),
+                Arguments.of("Usuario Valido", "usuario@email.com", "11911112222", "52998224725", " ", "rg blank")
+        );
+    }
 
 }
+
+
+
